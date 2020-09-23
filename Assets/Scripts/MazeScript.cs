f54d77f3aa159f16d0bb17dfc10d9163ecf40457
Rico Coelen <rico.coelen@gmail.com>
2020-09-23 20:02:48 +0200

    Initial commit, unity set-up and basic ui setup.

---
+++ Assets/Scripts/MazeScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeScript : MonoBehaviour
+{
+    public int Rows = 1;
+    public int Columns = 1;
+    private GameObject wall;
+    private GameObject floor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
