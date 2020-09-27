04a5c606964451c1852c65e9deb9dee99c7dcd4a
Rico Coelen <rico.coelen@gmail.com>
2020-09-27 17:53:22 +0200

    Made a game from mazegenerator and added alot of design and visuals, still want to implement more algorithims.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mazePanel;
+    [SerializeField]
+    private GameObject winPanel;
+    [SerializeField]
+    private GameObject MazeObject;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // freeze time and dont show winpanel yet
+        Time.timeScale = 0;
+        winPanel.SetActive(false);
+    }
+
+    public void Win()
+    {
+        // freeze time and show win
+        Time.timeScale = 0;
+        winPanel.SetActive(true);
+    }
+
+    public void Play()
+    {
+        // unfreeze time and let player reach end
+        mazePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Retry()
+    {
+        // return menu
+        winPanel.SetActive(false);
+        mazePanel.SetActive(true);
+        MazeObject.GetComponent<MazeScript>().Regenerate();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    public float speed = 1;
+    private Rigidbody rb;
+    private Vector3 movement;
+
+    [SerializeField]
+    private GameManager gameManager;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // get input without lag
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+    private void FixedUpdate()
+    {
+        // move rigidbody with physics
+        rb.MovePosition(transform.position + (movement * speed * Time.deltaTime));
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        // if collide with last floor make player win
+        if(col.gameObject.name == "EndFloor")
+        {
+            Debug.Log("You Win!");
+            gameManager.Win();
+        }
     }
 }
